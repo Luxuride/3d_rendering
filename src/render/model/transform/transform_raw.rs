7975fb8e896d880083ac041efcdc11118296a1cf
Luxuride <luxuridegaming@gmail.com>
2025-06-02 01:14:57 +0900

    mend

--- src/render/model/transform/transform_raw.rs
+++ src/render/model/transform/transform_raw.rs
@@ -7,16 +7,6 @@ pub struct TransformRaw {
     pub model: [[f32; 4]; 4],
 }
 impl TransformRaw {
-    const ATTRIBS: [wgpu::VertexAttribute; 4] =
-        wgpu::vertex_attr_array![5 => Float32x4, 6 => Float32x4, 7 => Float32x4, 8 => Float32x4];
-    pub fn desc() -> wgpu::VertexBufferLayout<'static> {
-        wgpu::VertexBufferLayout {
-            array_stride: size_of::<TransformRaw>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Instance,
-            attributes: &Self::ATTRIBS,
-        }
-    }
-
     pub fn transform_bind_group_layout(device: &Device) -> BindGroupLayout {
         device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
             entries: &[wgpu::BindGroupLayoutEntry {
